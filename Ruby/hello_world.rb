aaa48927d046429250b33241c3d4c7e42ca297ad
Hongbo Wei <hongbo_wei@outlook.com>
2024-08-21 00:19:21 +0400

    Create hello_world.rb

---
+++ Ruby/hello_world.rb
@@ -0,0 +1,7 @@
+puts 'Hello, World!'
+
+# To run the Ruby script, run the following command in your terminal:
+# ruby hello_world.rb
+
+# To check the version of Ruby installed on your system, run the following command in your terminal:
+# ruby -v
